84c3681807a6c8451389e6df6e96fdeee70e5ef0
Vukani Gcabashe <gcabashevukani3@gmail.com>
2023-09-17 19:40:26 -1200

    fixing build errors and adding deploy stuff

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,36 +1,35 @@
-import express from 'express';
+// import express from 'express';
 
-import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
-import registerUser from './UsersController';
-import posts from './postsController';
-import mongoose from 'mongoose';
-// import { gamesRouter } from '../routes/games.router';
+// import MessageResponse from '../interfaces/MessageResponse';
+// import emojis from './emojis';
+// import registerUser from './UsersController';
+// import posts from './postsController';
+// import mongoose from 'mongoose';
+// // import { gamesRouter } from '../routes/games.router';
 
-const router = express.Router();
-const uri = 'mongodb+srv://gcabashevukani3:54zIDSLibmMegl5f@cluster0.uo5k1du.mongodb.net/users?retryWrites=true&w=majority';
+// const router = express.Router();
+// const uri = 'mongodb+srv://gcabashevukani3:54zIDSLibmMegl5f@cluster0.uo5k1du.mongodb.net/users?retryWrites=true&w=majority';
 
-router.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏',
-  });
-});
+// router.get<{}, MessageResponse>('/', (req, res) => {
+//   res.json({
+//     message: 'API - 👋🌎🌍🌏',
+//   });
+// });
 
-mongoose.connect(uri).then(() => {
-  console.log('\nConnected to DB');
-}).catch((error) => {
-  console.log('connection error', error);
-});
+// mongoose.connect(uri).then(() => {
+//   console.log('\nConnected to DB');
+// }).catch((error) => {
+//   console.log('connection error', error);
+// });
 
-router.use('/users', registerUser);
+// router.use('/users', registerUser);
 
-router.use('/emojis', emojis);
+// router.use('/emojis', emojis);
 
-router.use('/posts', posts);
-router.use('/test', posts);
-// router.use('/games', gamesRouter);
+// router.use('/posts', posts);
+// // router.use('/games', gamesRouter);
 
-export default router;
+// export default router;
 
 
-// 'mongodb+srv://gcabashevukani3:54zIDSLibmMegl5f@cluster0.uo5k1du.mongodb.net/users?retryWrites=true&w=majority';
+// // 'mongodb+srv://gcabashevukani3:54zIDSLibmMegl5f@cluster0.uo5k1du.mongodb.net/users?retryWrites=true&w=majority';
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { connectToDatabase } from './services/database.service';
 import { userRouter } from './routes/users.router';
 import postsRouter from './routes/posts.router';
+import emojis from './controllers/emojis';
 
 const app = express();
 const cors = require('cors');
@@ -15,6 +16,9 @@ connectToDatabase().then(() => {
   app.use('/posts', postsRouter);
   app.use('/user', userRouter);
   
+  // testing:
+  
+  app.use('/emojis', emojis);
   app.listen('5000', () => {
     console.log(`Server started at http://localhost:${5000}`);
   });
